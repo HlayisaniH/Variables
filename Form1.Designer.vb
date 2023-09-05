b9f9547bd0419fee60b531df42a8c53a483dd900
HlayisaniH <90471779+HlayisaniH@users.noreply.github.com>
2023-09-05 17:31:40 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.fourbtn = New System.Windows.Forms.Button()
+        Me.fivebtn = New System.Windows.Forms.Button()
+        Me.sixbtn = New System.Windows.Forms.Button()
+        Me.ninebtn = New System.Windows.Forms.Button()
+        Me.eightbtn = New System.Windows.Forms.Button()
+        Me.sevenbtn = New System.Windows.Forms.Button()
+        Me.threebtn = New System.Windows.Forms.Button()
+        Me.twobtn = New System.Windows.Forms.Button()
+        Me.onebtn = New System.Windows.Forms.Button()
+        Me.plusbtn = New System.Windows.Forms.Button()
+        Me.equalbtn = New System.Windows.Forms.Button()
+        Me.divisionbtn = New System.Windows.Forms.Button()
+        Me.multiplybtn = New System.Windows.Forms.Button()
+        Me.subtractbtn = New System.Windows.Forms.Button()
+        Me.zerobtn = New System.Windows.Forms.Button()
+        Me.commabtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'fourbtn
+        '
+        Me.fourbtn.Location = New System.Drawing.Point(290, 222)
+        Me.fourbtn.Name = "fourbtn"
+        Me.fourbtn.Size = New System.Drawing.Size(90, 34)
+        Me.fourbtn.TabIndex = 0
+        Me.fourbtn.Text = "4"
+        Me.fourbtn.UseVisualStyleBackColor = True
+        '
+        'fivebtn
+        '
+        Me.fivebtn.Location = New System.Drawing.Point(396, 222)
+        Me.fivebtn.Name = "fivebtn"
+        Me.fivebtn.Size = New System.Drawing.Size(90, 34)
+        Me.fivebtn.TabIndex = 1
+        Me.fivebtn.Text = "5"
+        Me.fivebtn.UseVisualStyleBackColor = True
+        '
+        'sixbtn
+        '
+        Me.sixbtn.Location = New System.Drawing.Point(504, 222)
+        Me.sixbtn.Name = "sixbtn"
+        Me.sixbtn.Size = New System.Drawing.Size(90, 34)
+        Me.sixbtn.TabIndex = 2
+        Me.sixbtn.Text = "6"
+        Me.sixbtn.UseVisualStyleBackColor = True
+        '
+        'ninebtn
+        '
+        Me.ninebtn.Location = New System.Drawing.Point(504, 168)
+        Me.ninebtn.Name = "ninebtn"
+        Me.ninebtn.Size = New System.Drawing.Size(90, 34)
+        Me.ninebtn.TabIndex = 3
+        Me.ninebtn.Text = "9"
+        Me.ninebtn.UseVisualStyleBackColor = True
+        '
+        'eightbtn
+        '
+        Me.eightbtn.Location = New System.Drawing.Point(396, 168)
+        Me.eightbtn.Name = "eightbtn"
+        Me.eightbtn.Size = New System.Drawing.Size(90, 34)
+        Me.eightbtn.TabIndex = 4
+        Me.eightbtn.Text = "8"
+        Me.eightbtn.UseVisualStyleBackColor = True
+        '
+        'sevenbtn
+        '
+        Me.sevenbtn.Location = New System.Drawing.Point(290, 168)
+        Me.sevenbtn.Name = "sevenbtn"
+        Me.sevenbtn.Size = New System.Drawing.Size(90, 34)
+        Me.sevenbtn.TabIndex = 5
+        Me.sevenbtn.Text = "7"
+        Me.sevenbtn.UseVisualStyleBackColor = True
+        '
+        'threebtn
+        '
+        Me.threebtn.Location = New System.Drawing.Point(504, 277)
+        Me.threebtn.Name = "threebtn"
+        Me.threebtn.Size = New System.Drawing.Size(90, 34)
+        Me.threebtn.TabIndex = 6
+        Me.threebtn.Text = "3"
+        Me.threebtn.UseVisualStyleBackColor = True
+        '
+        'twobtn
+        '
+        Me.twobtn.Location = New System.Drawing.Point(396, 277)
+        Me.twobtn.Name = "twobtn"
+        Me.twobtn.Size = New System.Drawing.Size(90, 34)
+        Me.twobtn.TabIndex = 7
+        Me.twobtn.Text = "2"
+        Me.twobtn.UseVisualStyleBackColor = True
+        '
+        'onebtn
+        '
+        Me.onebtn.Location = New System.Drawing.Point(290, 277)
+        Me.onebtn.Name = "onebtn"
+        Me.onebtn.Size = New System.Drawing.Size(90, 34)
+        Me.onebtn.TabIndex = 8
+        Me.onebtn.Text = "1"
+        Me.onebtn.UseVisualStyleBackColor = True
+        '
+        'plusbtn
+        '
+        Me.plusbtn.Location = New System.Drawing.Point(612, 277)
+        Me.plusbtn.Name = "plusbtn"
+        Me.plusbtn.Size = New System.Drawing.Size(90, 34)
+        Me.plusbtn.TabIndex = 9
+        Me.plusbtn.Text = "+"
+        Me.plusbtn.UseVisualStyleBackColor = True
+        '
+        'equalbtn
+        '
+        Me.equalbtn.Location = New System.Drawing.Point(504, 326)
+        Me.equalbtn.Name = "equalbtn"
+        Me.equalbtn.Size = New System.Drawing.Size(198, 34)
+        Me.equalbtn.TabIndex = 10
+        Me.equalbtn.Text = "="
+        Me.equalbtn.UseVisualStyleBackColor = True
+        '
+        'divisionbtn
+        '
+        Me.divisionbtn.Location = New System.Drawing.Point(612, 116)
+        Me.divisionbtn.Name = "divisionbtn"
+        Me.divisionbtn.Size = New System.Drawing.Size(90, 34)
+        Me.divisionbtn.TabIndex = 11
+        Me.divisionbtn.Text = "/"
+        Me.divisionbtn.UseVisualStyleBackColor = True
+        '
+        'multiplybtn
+        '
+        Me.multiplybtn.Location = New System.Drawing.Point(612, 168)
+        Me.multiplybtn.Name = "multiplybtn"
+        Me.multiplybtn.Size = New System.Drawing.Size(90, 34)
+        Me.multiplybtn.TabIndex = 12
+        Me.multiplybtn.Text = "*"
+        Me.multiplybtn.UseVisualStyleBackColor = True
+        '
+        'subtractbtn
+        '
+        Me.subtractbtn.Location = New System.Drawing.Point(612, 224)
+        Me.subtractbtn.Name = "subtractbtn"
+        Me.subtractbtn.Size = New System.Drawing.Size(90, 34)
+        Me.subtractbtn.TabIndex = 13
+        Me.subtractbtn.Text = "-"
+        Me.subtractbtn.UseVisualStyleBackColor = True
+        '
+        'zerobtn
+        '
+        Me.zerobtn.Location = New System.Drawing.Point(290, 326)
+        Me.zerobtn.Name = "zerobtn"
+        Me.zerobtn.Size = New System.Drawing.Size(90, 34)
+        Me.zerobtn.TabIndex = 14
+        Me.zerobtn.Text = "0"
+        Me.zerobtn.UseVisualStyleBackColor = True
+        '
+        'commabtn
+        '
+        Me.commabtn.Location = New System.Drawing.Point(396, 326)
+        Me.commabtn.Name = "commabtn"
+        Me.commabtn.Size = New System.Drawing.Size(90, 34)
+        Me.commabtn.TabIndex = 15
+        Me.commabtn.Text = "."
+        Me.commabtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.commabtn)
+        Me.Controls.Add(Me.zerobtn)
+        Me.Controls.Add(Me.subtractbtn)
+        Me.Controls.Add(Me.multiplybtn)
+        Me.Controls.Add(Me.divisionbtn)
+        Me.Controls.Add(Me.equalbtn)
+        Me.Controls.Add(Me.plusbtn)
+        Me.Controls.Add(Me.onebtn)
+        Me.Controls.Add(Me.twobtn)
+        Me.Controls.Add(Me.threebtn)
+        Me.Controls.Add(Me.sevenbtn)
+        Me.Controls.Add(Me.eightbtn)
+        Me.Controls.Add(Me.ninebtn)
+        Me.Controls.Add(Me.sixbtn)
+        Me.Controls.Add(Me.fivebtn)
+        Me.Controls.Add(Me.fourbtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents fourbtn As Button
+    Friend WithEvents fivebtn As Button
+    Friend WithEvents sixbtn As Button
+    Friend WithEvents ninebtn As Button
+    Friend WithEvents eightbtn As Button
+    Friend WithEvents sevenbtn As Button
+    Friend WithEvents threebtn As Button
+    Friend WithEvents twobtn As Button
+    Friend WithEvents onebtn As Button
+    Friend WithEvents plusbtn As Button
+    Friend WithEvents equalbtn As Button
+    Friend WithEvents divisionbtn As Button
+    Friend WithEvents multiplybtn As Button
+    Friend WithEvents subtractbtn As Button
+    Friend WithEvents zerobtn As Button
+    Friend WithEvents commabtn As Button
+End Class
